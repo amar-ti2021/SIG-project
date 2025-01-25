8bb7caacd444e2b375e50f53c85e0e7ca14cae98
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2025-01-25 07:55:29 +0700

    feat: create Homepage

---
+++ src/page/home/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import HeroSection from "../../../components/home/HeroSection";
+import MapSection from "../../../components/home/MapSection";
+import Navbar from "../../../components/home/Navbar";
+
+const Homepage = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <MapSection />
+    </>
+  );
+};
+
+export default Homepage;
